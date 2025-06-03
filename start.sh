9623379e6c929320adbb31745601bf4767a38ac4
Cherry <isaac.corrales0304@gmail.com>
2025-06-03 01:14:58 -0600

    Nuevo bash para agilizar en producción

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activar entorno virtual
+source ./venv/scripts/activate
+
+# Ejecutar el servidor Python
+python main.py
+
+# Evitar que la ventana se cierre automáticamente (esperar entrada del usuario)
+read -p "Presiona Enter para salir..."
